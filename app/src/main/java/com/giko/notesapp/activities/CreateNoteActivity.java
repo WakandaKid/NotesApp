497db32e01c55d69bb23017e48577f917d06fdaa
Gideon Ukoko Jr <gideon.ukoko98@gmail.com>
2021-07-05 05:38:22 +0100

    Implemented Creation and Saving of Notes to RoomDB

--- app/src/main/java/com/giko/notesapp/activities/CreateNoteActivity.java
+++ app/src/main/java/com/giko/notesapp/activities/CreateNoteActivity.java
@@ -2,18 +2,43 @@ package com.giko.notesapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.giko.notesapp.R;
+import com.giko.notesapp.database.NotesDB;
+import com.giko.notesapp.entities.Note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateNoteActivity extends AppCompatActivity {
+
+    private EditText inputNoteTitle, inputNoteSubtitle, inputNoteText;
+    private TextView txtDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
+
+        inputNoteTitle = findViewById(R.id.inputNoteTitle);
+        inputNoteSubtitle = findViewById(R.id.inputNoteSubtitle);
+        inputNoteText = findViewById(R.id.inputNote);
+
+        txtDateTime = findViewById(R.id.txtDateTime);
+        txtDateTime.setText(
+                new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault())
+                .format(new Date())
+        );
 
         ImageView imgBack = findViewById(R.id.imgBack);
 
@@ -23,5 +48,53 @@ public class CreateNoteActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        ImageView imgSave = findViewById(R.id.imgSave);
+
+        imgSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveNote();
+            }
+        });
+    }
+
+    private void saveNote(){
+        if (inputNoteTitle.getText().toString().trim().isEmpty()){
+            Toast.makeText(this, "Please Fill in the Note Title Section", Toast.LENGTH_SHORT).show();
+            return;
+        }else if (inputNoteSubtitle.getText().toString().trim().isEmpty()){
+            inputNoteSubtitle.setText(R.string.empty_subtitle);
+        }else if (inputNoteText.getText().toString().trim().isEmpty()){
+            Toast.makeText(this, "Please fill in the Note Section", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        final Note note = new Note();
+        note.setTitle(inputNoteTitle.getText().toString().trim());
+        note.setSubtitle(inputNoteSubtitle.getText().toString().trim());
+        note.setNoteText(inputNoteText.getText().toString().trim());
+        note.setDateTime(txtDateTime.getText().toString());
+
+        @SuppressLint("StaticFieldLeak")
+        class SaveNoteTask extends AsyncTask<Void, Void, Void>{
+
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                NotesDB.getDatabase(getApplicationContext()).noteDao().insertNote(note);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void unused) {
+                super.onPostExecute(unused);
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        }
+
+        new SaveNoteTask().execute();
     }
 }
